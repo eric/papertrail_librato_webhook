5a506ff5f2f79ebb7cff69c18db24b2ec8cb07a0
Eric Lindvall <eric@5stops.com>
2011-06-06 00:42:10 -0700

    Enable bundler.

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source :rubygems
 gem 'sinatra',   '~> 1.0'
 gem 'yajl-ruby', '~> 0.7.8'
 gem 'faraday'
-gem 'activesupport', '~> 2.3'
+gem 'activesupport', '~> 2.3', :require => 'active_support'
 gem 'always_verify_ssl_certificates'
--- app/libraries.rb
+++ app/libraries.rb
@@ -1,6 +1,9 @@
 RACK_ENV  = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
 RACK_ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
 
+require 'bundler'
+Bundler.require :default
+
 require 'sinatra/base'
 require 'yajl'
 require 'faraday'
