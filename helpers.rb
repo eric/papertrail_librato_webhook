197b211adeefd52ed069f98c492d464416306405
Eric Lindvall <eric@5stops.com>
2011-06-06 15:03:04 -0700

    Make SSL work on heroku.

--- helpers.rb
+++ helpers.rb
@@ -3,6 +3,13 @@ module PapertrailSilverlineWebhook
     def faraday(options = {})
       @faraday ||= begin
         options[:timeout] ||= 6
+
+        # Make SSL work on heroku
+        if File.exists?('/usr/lib/ssl/certs/ca-certificates.crt')
+          options[:ssl] ||= {}
+          options[:ssl][:ca_path] = '/usr/lib/ssl/certs/ca-certificates.crt'
+        end
+
         Faraday.new(options) do |b|
           b.request :url_encoded
           b.request :json
